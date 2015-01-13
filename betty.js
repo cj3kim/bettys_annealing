63042f282d884ea1e1fcaae26f1942a04652737c
Chris Kim <cj3kim@gmail.com>
2015-01-13 01:28:22 -0800

    Initial Commit

---
+++ betty.js
@@ -0,0 +1,32 @@
+
+
+
+define(['d3'], function (d3) {
+
+  function betty()  {
+    var beta = 0.1;
+    var subtractor = 0.0001;
+
+    this.start = function () {
+      setInterval(function () {
+        if (beta > 0) beta -= subtractor;
+        if (beta < 0) beta = 0;
+      }, 1);
+    };
+
+    this.beta = function () {
+      if (arguments.length > 0) {
+        var _beta = arguments[0];
+        if (_beta < 0.1) {
+          beta = _beta;
+        } else {
+          beta = 0.1;
+        }
+      } 
+      return beta;
+    };
+
+  }
+
+  return betty;
+})
